267a4123a4bd4c782e639adfaade520daaba89a7
giveback007 <dovy91@gmail.com>
2020-12-08 18:43:25 -0500

    added getUrlParams

--- src/general.ts
+++ src/general.ts
@@ -1,3 +1,5 @@
+import type { Dict } from '@giveback007/util-lib';
+
 export const copyToClipboard = (str: string) => navigator.clipboard.writeText(str);
 
 export const viewSize = ({ innerWidth, innerHeight } = window) =>
@@ -9,4 +11,22 @@ export function elmById(id: string)
     if (!el) throw Error('no element with id: ' + id);
 
     return el;
+};
+
+
+// function inspired by this discussion:
+// https://gist.github.com/pirate/9298155edda679510723
+export function getUrlParams(loc: Location = window.location) {
+    const { origin, pathname, hash, search } = loc;
+    const str = '' + hash ? hash : '' + search ? search : '';
+
+    const hashes = str.slice(search.indexOf('?') + 1).split('&');
+    const params: Dict<string | undefined> = hashes.reduce((obj, x) => {
+      const [key, val] = x.split('=');
+      if (!key || !val) return obj;
+
+      return { ...obj, [key]: decodeURIComponent(val) };
+    }, { });
+
+    return { origin, pathname, params };
 };
